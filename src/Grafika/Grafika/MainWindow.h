e7f042596c9e3bbf0494529997fcd9a7de891e5f
Lev.Chiryatev <lev.chiryatev@gmail.com>
2024-01-06 15:31:34 +0700

    #1 review changes

--- src/Grafika/Grafika/MainWindow.h
+++ src/Grafika/Grafika/MainWindow.h
@@ -4,19 +4,14 @@
 
 #include <QtWidgets/QMainWindow>
 
-
 namespace Ui { class MainWindowClass; };
-
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
     std::unique_ptr<Ui::MainWindowClass> ui;
-
 };
