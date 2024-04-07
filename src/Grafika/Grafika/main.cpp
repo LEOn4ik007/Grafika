bbdd9f79d7754aef60608b8d732740447080eebc
Lev.Chiryatev <lev.chiryatev@gmail.com>
2024-04-07 13:17:33 +0700

    #54 Settings manager added

--- src/Grafika/Grafika/main.cpp
+++ src/Grafika/Grafika/main.cpp
@@ -1,9 +1,25 @@
 #include "MainWindow.h"
 #include <QtWidgets/QApplication>
+#include <QTranslator>
+#include <QSettings>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    const auto pathTemplate = QString("%1/%2").arg(a.applicationDirPath(), "%1");
+
+    QSettings settings(pathTemplate.arg("Grafika.ini"), QSettings::Format::IniFormat);
+    auto lang = settings.value("lang").toString();
+    if (lang.isEmpty())
+        lang = "en";
+    
+    QTranslator translator;
+    QString translationPath = pathTemplate.arg(QString("%1.qm").arg(lang));
+    if (translator.load(translationPath))
+        a.installTranslator(&translator);
+    else
+        assert(false && "Cant load translation file");
+      
     MainWindow w;
     w.show();
     return a.exec();
